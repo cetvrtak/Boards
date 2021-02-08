fe5ee200f159630777e35c9324a139fbbe29d763
cetvrtak <cetvrtak@github.com>
2021-02-08 13:31:35 +0100

    Redirect to localhost:4000/boards/1 on login

--- config/config.exs
+++ config/config.exs
@@ -29,7 +29,8 @@ config :phoenix, :json_library, Jason
 config :vision, :pow,
   user: Vision.Users.User,
   repo: Vision.Repo,
-  web_module: VisionWeb
+  web_module: VisionWeb,
+  routes_backend: VisionWeb.Pow.Routes
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
